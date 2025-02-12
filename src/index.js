74c898103962056a373253af6158cafe55e21b52
liamcottle <liam@liamcottle.com>
2025-02-13 00:18:25 +1300

    export classes

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import Connection from "./connection/connection.js";
+import BleConnection from "./connection/ble_connection.js";
+import SerialConnection from "./connection/serial_connection.js";
+import Constants from "./constants.js";
+
+export {
+    Connection,
+    BleConnection,
+    SerialConnection,
+    Constants,
+};

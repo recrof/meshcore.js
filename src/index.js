2f1c696842c4f4dc81280cf755bc09f150e03598
liamcottle <liam@liamcottle.com>
2025-02-15 21:41:30 +1300

    export packet class

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import Connection from "./connection/connection.js";
 import BleConnection from "./connection/ble_connection.js";
 import SerialConnection from "./connection/serial_connection.js";
 import Constants from "./constants.js";
+import Packet from "./packet.js";
 
 export {
     Connection,
     BleConnection,
     SerialConnection,
     Constants,
+    Packet,
 };

4fc0aad0dff4370db661dbaafd8aea6735cb4ee4
liamcottle <liam@liamcottle.com>
2025-03-04 16:49:43 +1300

    export tcp connection class from lib

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Connection from "./connection/connection.js";
 import BleConnection from "./connection/ble_connection.js";
 import SerialConnection from "./connection/serial_connection.js";
+import TCPConnection from "./connection/tcp_connection.js";
 import Constants from "./constants.js";
 import Packet from "./packet.js";
 
@@ -8,6 +9,7 @@ export {
     Connection,
     BleConnection,
     SerialConnection,
+    TCPConnection,
     Constants,
     Packet,
 };

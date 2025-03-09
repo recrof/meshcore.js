bc11e6a7fee20f2cfeeaa37253cebed46f0ca5d9
liamcottle <liam@liamcottle.com>
2025-03-09 15:42:11 +1300

    add packet parser example

---
+++ examples/parse_packet.js
@@ -0,0 +1,6 @@
+import Packet from "../src/packet.js";
+
+const bytes = Buffer.from("0200B401DF6528CC9778A56F36FE9399A5CF6B0C7EDE", "hex");
+const packet = Packet.fromBytes(bytes);
+
+console.log(packet);

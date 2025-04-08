7ec82f8554e429496916fceb836485e8211bdef7
liamcottle <liam@liamcottle.com>
2025-04-08 12:15:46 +1200

    export new connection classes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import Connection from "./connection/connection.js";
 import BleConnection from "./connection/ble_connection.js";
 import SerialConnection from "./connection/serial_connection.js";
+import NodeJSSerialConnection from "./connection/nodejs_serial_connection.js";
+import WebSerialConnection from "./connection/web_serial_connection.js";
 import TCPConnection from "./connection/tcp_connection.js";
 import Constants from "./constants.js";
 import Advert from "./advert.js";
@@ -11,6 +13,8 @@ export {
     Connection,
     BleConnection,
     SerialConnection,
+    NodeJSSerialConnection,
+    WebSerialConnection,
     TCPConnection,
     Constants,
     Advert,
